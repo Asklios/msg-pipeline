c0cd6f959e4d5b134f320793099b57852d0d818c
Asklios <asklios@asklios.de>
2022-05-11 16:52:09 +0200

    Fix env names in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ RESTPLUS_SWAGGER_EXPANSION = os.environ.get('RESTPLUS_SWAGGER_EXPANSION', 'list'
 RESTPLUS_VAL = os.environ.get('RESTPLUS_VAL', True)
 RESTPLUS_MASK_SWAGGER = os.environ.get('RESTPLUS_MASK_SWAGGER', False)
 
-RECEIVE_TOKEN = os.environ.get('CLIENT_TOKEN', 'test_token')
-SEND_TOKEN = os.environ.get('CLIENT_TOKEN', 'test_token')
+RECEIVE_TOKEN = os.environ.get('RECEIVE_TOKEN', 'test_token')
+SEND_TOKEN = os.environ.get('SEND_TOKEN', 'test_token')
 
 EXPORT_SWAGGER_FILE = os.environ.get('EXPORT_SWAGGER_FILE') is not None
